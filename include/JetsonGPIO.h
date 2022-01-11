9483a2242b65b2f9fc8c62b5fbfab6a145b926de
jueon.park <bluegbgb@gmail.com>
2022-01-11 21:40:02 +0900

    update version info (1.0.4) - Should be equivalent to v2.0.12 of the official python library.

--- include/JetsonGPIO.h
+++ include/JetsonGPIO.h
@@ -62,7 +62,7 @@ namespace std
 
 namespace GPIO
 {
-   constexpr auto VERSION = "1.0.3";
+   constexpr auto VERSION = "1.0.4";
    constexpr auto _SYSFS_ROOT = "/sys/class/gpio";
 
    extern const std::string JETSON_INFO;
