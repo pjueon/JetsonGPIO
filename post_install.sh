69c101a140ce821260c2fec29e8488d732c6e881
jueon.park <bluegbgb@gmail.com>
2022-01-26 21:47:50 +0900

    update post_install.sh - add log messages and comments

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# In order to use the Jetson GPIO Library, the correct user permissions/groups must be set.
+echo "[Setting user permissions]"
 echo "Creating group gpio..."
 groupadd -f -r gpio
 LOGNAME=$(logname)
